cb76c30f89c7f02f938149c0c7323e326b9232af
Stain-h <sangheon1646@naver.com>
2021-09-21 13:57:46 +0900

    add : 상품등록로직

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,10 +1,56 @@
+import useOrders from '../hooks/useOrders'
+import usePrototypes from '../hooks/usePrototypes'
+import useActions from '../hooks/useActions'
+
 export default function Orders() {
+  const orders = useOrders();
+  const prototypes = usePrototypes();
+  const { remove } = useActions();
+  
+  if(orders.length === 0){
+    return (
+      <aside>
+        <div className="empty">
+          <div className="title">You don't have any orders</div>
+          <div className="subtitle">click on a + to add an order</div>
+        </div>
+      </aside>
+    )
+  }
+
   return (
     <aside>
-      <div className="empty">
-        <div className="title">You don't have any orders</div>
-        <div className="subtitle">click on a + to add an order</div>
+      <div className="order">
+        <div className="body">
+          {orders.map((order) => {
+            const { id } = order;
+            const click = () => {
+              remove(id);
+            }
+            // order.id === prototypes.id  
+            const prototype  = prototypes.find((p) => id === p.id)
+  
+            return (
+              <div className="item" key={id}>
+                <div className="img">
+                  <video src={prototype.thumbnail}></video>
+                </div>
+                <div className="content">
+                  <p className="title">
+                    {prototype.title} x {order.quantity}
+                  </p>
+                </div>
+                <div className="action">
+                  <p className="price">$ {prototype.price * order.quantity}</p>
+                  <button className="btn btn--link" onClick={click}>
+                    <i className="icon icon--plus"></i>
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </aside>
-  )
+  );
 }
--- src/provider/AppStateProvider.jsx
+++ src/provider/AppStateProvider.jsx
@@ -6,10 +6,34 @@ const AppStateProvider = ({ children }) => {
   const [prototypes, setPrototypes] = useState(data);
   const [orders, setOrders] = useState([]);
   
+  // { id, quantity : 1 }
+  // orders에 id가 존재한다면 quantity만 +1 
+  // orders에 id가 존재하지 않는다면 id : id, quantity : 1 
+
   const addToOrder = useCallback((id) => {
+    setOrders((orders) => {
+      const finded = orders.find((order) => order.id === id);
+      if(finded === undefined) {
+        return [...orders, { id, quantity: 1 }];
+      }else {
+        return orders.map((order) => {
+          if(order.id === id){
+            return { 
+              id,
+              quantity: order.quantity + 1
+            };
+          }else {
+            return order;
+          }
+        })
+      }
+    })
+  },[])
+
+  const remove = useCallback((id) => {
     console.log(id);
   },[])
-  const remove = useCallback((id) => {},[])
+  
   const removeAll = useCallback(() => {},[])
 
   return (
